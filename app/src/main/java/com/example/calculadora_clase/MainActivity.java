b0e19287657aeda9f904c2e92954da1c64f72b7a
Leandro <leandro9115@gmail.com>
2019-07-06 10:32:00 -0500

    commit incial

---
+++ app/src/main/java/com/example/calculadora_clase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.calculadora_clase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
